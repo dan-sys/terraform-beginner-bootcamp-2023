10ee5992085b92f66a99d4a4ab65263d2239ce46
Daniel Romooluwa <60560440+dan-sys@users.noreply.github.com>
2023-10-28 21:10:18 +0200

    10 simple s3 bucket creation (#11)
    * s3 bucket setup
    
    * 10 - install aws terraform provider to create s3 buckets with automated names from the random string provider

--- main.tf
+++ main.tf
@@ -4,7 +4,15 @@ terraform {
       source = "hashicorp/random"
       version = "3.5.1"
     }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.23.1"
+    }
   }
+}
+
+provider "aws" {
+  # Configuration options
 }
 
 provider "random" {
@@ -12,8 +20,15 @@ provider "random" {
 }
 
 resource "random_string" "bucket_name" {
-  length           = 16
-  special          = false
+  length= 24
+  lower = true
+  upper = false
+  special= false
+}
+
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
+resource "aws_s3_bucket" "example" {
+  bucket = random_string.bucket_name.result
 }
 
 output "random_bucket_name" {

a38b582716f60fcc1d601c609da292fb37d5af1e
Daniel Romooluwa <60560440+dan-sys@users.noreply.github.com>
2024-02-22 05:10:27 +0100

    #10 used a tf provider to generate a random string bucket name (#11)

--- main.tf
+++ main.tf
@@ -1,1 +1,23 @@
 
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.6.0"
+    }
+  }
+}
+
+provider "random" {
+  # Configuration options
+}
+
+resource "random_string" "rnd_bucket_name" {
+  length           = 16
+  special          = false
+  override_special = ""
+}
+
+output "rand_bucket_name"{
+    value = random_string.rnd_bucket_name.result
+}

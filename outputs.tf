636f4a4ed2967e780b4c9c8ed5d6cadcc3655a58
Daniel Romooluwa <60560440+dan-sys@users.noreply.github.com>
2024-03-12 05:21:20 +0100

    40 terratowns provider (#42)
    * Work in progress to create a custom terraform provider from scratch.
    
    * setting up provider block in main.tf file,tf plan and apply now work seamlessly

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,15 @@
 
-output "bucket_name"{
-    description="Bucket name for the static website bucket"
-    value = module.terrahouse_aws.bucket_name
-}
-
-output "s3_website_endpoint"{
-    description="Static Bucket endpoint"
-    value = module.terrahouse_aws.website_endpoint
-}
-
-output "cloudfront_url"{
-    description="Cloudfront distro doamian name"
-    value = module.terrahouse_aws.cloudfront_url
-}
+#output "bucket_name"{
+#    description="Bucket name for the static website bucket"
+#    value = module.terrahouse_aws.bucket_name
+#}
+#
+#output "s3_website_endpoint"{
+#    description="Static Bucket endpoint"
+#    value = module.terrahouse_aws.website_endpoint
+#}
+#
+#output "cloudfront_url"{
+#    description="Cloudfront distro doamian name"
+#    value = module.terrahouse_aws.cloudfront_url
+#}
